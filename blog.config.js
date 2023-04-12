7865108793cbc8f7f4376edd3b1ab79f8de01e42
Johnnie Walker <rockwave.fm@gmail.com>
2023-04-12 13:55:22 +0700

    Personalized template

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Evgenii Chernyshov',
   email: 'rockwave.fm@gmail.com',
   link: 'https://swiftwalker.vercel.app',
-  description: 'SwiftWalker - a blog where I post some notes about iOS programming',
+  description: 'Notes about iOS programming',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Bangkok', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'dark', // ['light', 'dark', 'auto'],
@@ -17,7 +17,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app/**Swift**Walker.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg&widths=auto', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/tyneverslezam',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
