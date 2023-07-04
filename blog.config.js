10affdd8cc434bf4f7f5557bb356447f54fd1917
Johnnie Walker <rockwave.fm@gmail.com>
2023-07-04 12:42:37 +0300

    Try to fix gitalk

--- blog.config.js
+++ blog.config.js
@@ -40,7 +40,7 @@ const BLOG = {
     // support provider: gitalk, utterances, cusdis
     provider: 'gitalk', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
-      repo: 'https://github.com/johnnieWa1ker/nobelium', // The repository of store comments
+      repo: 'nobelium', // The repository of store comments
       owner: 'johnnieWa1ker',
       admin: ['johnnieWa1ker'],
       clientID: '11eadb3b41c3db9dd87c',
